6cca7841bda86a525dbbb9252ae5ca644116ede1
Esoteric918 <3411@holbertonschool.com>
2022-08-15 11:16:34 -0500

    task 0 - 3

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,83 @@
-const graphql = require('graphql');
-const { GraphQLString,  GraphQLInt } = graphql;
+const graphql = require('graphql'),
+	{ GraphQLString,  GraphQLInt } = graphql;
+
+//require the module: lodash
+const _ = require('lodash');
+
+const tasks = [
+	{	id: '1',
+		title: 'Create your first webpage',
+		weight: 1,
+		description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+	},
+	{	id: '2',
+		title: 'Create your first webpage',
+		weight: 1,
+		description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)'
+	},
+];
+
+const projects = [
+  {	id: '1',
+    title: 'Advanced HTML',
+     weight: 1,
+     description: 'Welcome to the Web Stack specialization. The 3 first projects will give you all basics of the Web development: HTML, CSS and Developer tools. In this project, you will learn how to use HTML tags to structure a web page. No CSS, no styling - don’t worry, the final page will be “ugly” it’s normal, it’s not the purpose of this project. Important note: details are important! lowercase vs uppercase / wrong letter… be careful!'
+    },
+  {	id: '2',
+    title: "Bootstrap",
+    weight: 1,
+    description: 'Bootstrap is a free and open-source front-end web framework for developing with HTML, CSS, and JS. It contains HTML, CSS, and JS components for faster and easier web development. Bootstrap is the world’s most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.'
+  }
+  ]
 
 const TaskType = new graphql.GraphQLObjectType({
-  name: 'Task',
-  fields: () => ({
-    id: { type: GraphQLInt },
-    description: { type: GraphQLString },
-    title: { type: GraphQLString },
-    weight: { type: GraphQLInt },
-  }),
+	name: 'Task',
+	fields: () => ({
+		id: { type: GraphQLInt },
+		description: { type: GraphQLString },
+		title: { type: GraphQLString },
+		weight: { type: GraphQLInt },
+    task: {
+      type: TaskType,
+      resolve: (parent, args) => {
+        return _.find(tasks, { id: parent.id })
+    }
+    }
+	}),
 });
+
+const ProjectType = new graphql.GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: graphql.GraphQLID },
+    title: { type: graphql.GraphQLString },
+    weight: { type: graphql.GraphQLInt },
+    description: { type: graphql.GraphQLString },
+    resolve: (parent, args) =>{
+      return _.find(tasks, {id: parent.id});
+    }
+  })
+});
+
+
+const RootQuery = new graphql.GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: () => ({
+    task: {
+      type: TaskType,
+      args: {
+        id: { type: graphql.GraphQLID }
+      },
+      resolve: (parent, args) => {
+        return _.find(tasks, { id: args.id });
+      }
+    },
+  }),
+})
+
+
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery,
+});
+
+// module.exports = schema;
