b4436a2321acdae95b4f7183392ab63989591772
Esoteric918 <3411@holbertonschool.com>
2022-08-17 22:39:22 -0500

    taskLisk and addTask updates

--- GraphQL_API/client/src/components/AddTask.js
+++ GraphQL_API/client/src/components/AddTask.js
@@ -2,106 +2,103 @@ import {
   useState,
   //useEffect
 } from "react";
-
-const gql = require("apollo-boost");
-const graphql = require("react-apollo").graphql;
+const { gql } = require('apollo-boost');
+const { graphql } = require('react-apollo');
 
 function AddTask(props) {
   const [inputs, setInputs] = useState({
-    title: "",
+    title: '',
     weight: 1,
-    description: "",
-    projectId: "",
+    description: '',
+    projectId: ''
   });
 
+
   const handleChange = (e) => {
-    const newInputs = {
-      ...inputs,
-    };
-    if (e.target.name === "weight")
-      newInputs[e.target.name] = parseInt(e.target.value);
-    else newInputs[e.target.name] = e.target.value;
-    setInputs(newInputs);
-  };
-  function displayProjects() {
-    //  console.log(props);
-    var data = props.data;
-    if (data.loading) {
-      return <option> Loading projects... </option>;
-    } else {
-      return data.projects.map((project) => {
-        return (
-          <option key={project.id} value={project.id}>
-            {" "}
-            {project.title}{" "}
-          </option>
-        );
-      });
-    }
+        const newInputs = {
+          ...inputs
+        };
+        if (e.target.name === "weight") newInputs[e.target.name] = parseInt(e.target.value)
+        else newInputs[e.target.name] = e.target.value
+        setInputs(newInputs)
   }
-  return (
-    <form
-      class="task"
-      id="add-task"
-      /*onSubmit = {...}*/
+
+  return ( <
+    form class = "task"
+    id = "add-task"
+    /*onSubmit = {...}*/ >
+    <
+    div className = "field" >
+    <
+    label > Task title: < /label> <
+    input type = "text"
+    name = "title"
+    onChange = {
+      handleChange
+    }
+    value = {
+      inputs.title
+    }
+    required /
     >
-      <div className="field">
-        <label> Task title: </label>{" "}
-        <input
-          type="text"
-          name="title"
-          onChange={handleChange}
-          value={inputs.title}
-          required
-        />
-      </div>{" "}
-      <div className="field">
-        <label> Weight: </label>{" "}
-        <input
-          type="number"
-          name="weight"
-          onChange={handleChange}
-          value={inputs.weight}
-          required
-        />
-      </div>
-      <div className="field">
-        <label> description: </label>{" "}
-        <textarea
-          name="description"
-          onChange={handleChange}
-          value={inputs.description}
-          required
-        />
-      </div>
-      <div className="field">
-        <label> Project: </label>{" "}
-        <select
-          name="projectId"
-          onChange={handleChange}
-          value={inputs.projectId}
-          required
-        >
-          {" "}
-          <option value="" selected="selected" disabled="disabled">
-            {" "}
-            Select project{" "}
-          </option>{" "}
-        </select>
-        {displayProjects()}
-      </div>
-      <button> + </button>{" "}
-    </form>
+    < /
+    div > <
+    div className = "field" >
+    <
+    label > Weight: < /label> <
+    input type = "number"
+    name = "weight"
+    onChange = {
+      handleChange
+    }
+    value = {
+      inputs.weight
+    }
+    required /
+    >
+    < /
+    div >
+    <
+    div className = "field" >
+    <
+    label > description: < /label> <
+    textarea name = "description"
+    onChange = {
+      handleChange
+    }
+    value = {
+      inputs.description
+    }
+    required /
+    >
+    < /
+    div >
+    <
+    div className = "field" >
+    <
+    label > Project: < /label> <
+    select name = "projectId"
+    onChange = {
+      handleChange
+    }
+    value = {
+      inputs.projectId
+    }
+    required > < option value = ""
+    selected = "selected"
+    disabled = "disabled" > Select project < /option>  < /
+    select > < /
+    div >
+    <
+    button > + < /button> < /
+    form >
   );
 }
 
 const getProjectsQuery = gql`
-  query projects {
-    projects {
-      id
-      title
-    }
-  }
-`;
+{
+  projects {
+    id
 
-export default graphql(getProjectsQuery)(AddTask);
+
+export default AddTask;
--- GraphQL_API/client/src/components/TaskList.js
+++ GraphQL_API/client/src/components/TaskList.js
@@ -1,54 +1,62 @@
 import {
   useState,
-//useEffect
- } from "react";
-
+  //useEffect
+} from "react";
 // components
-import TaskDetails from "./TaskDetails";
-import gql from "apollo-boost";
-const graphql = require("react-apollo");
+import TaskDetails from './TaskDetails';
+const { gql } = require('apollo-boost');
+const { graphql } = require('react-apollo');
+
+
 
 function TaskList(props) {
   const [state, setState] = useState({
-    selected: null,
+    selected: null
   });
 
+  // console.log(props);
   function displayTasks() {
     console.log(props.data);
     var data = props.data;
 
     if (data.loading) {
-      return <div> Loading tasks... </div>;
+      return ( <div> Loading tasks... </div> );
     } else {
-      return data.tasks.map((task) => {
+      return data.tasks.map(task => {
         return (
           <li
-            key={task.id}
-            onClick={(e) => {
+            key = {task.id}
+            onClick = {(e) => {
               setState({
-                selected: task.id,
+                selected: task.id
               });
             }}
           >
             {task.title}
           </li>
         );
-      });
+      })
     }
   }
   return (
     <div>
-      <ul id="task-list"> {displayTasks()} </ul>
-      <TaskDetails taskId={state.selected} />
+      <ul id = "task-list" >
+        {displayTasks()}
+      </ul>
+      <TaskDetails />
     </div>
   );
 }
 
-// query to get all tasks
+
 const getTasksQuery = gql`
-  query tasks {
-      id
-      title
-    }
-  }`;
+{
+  tasks {
+    id
+    title
+  }
+}
+`
+
+// export default TaskList;
 export default graphql(getTasksQuery)(TaskList);
